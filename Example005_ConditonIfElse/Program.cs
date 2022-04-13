84432de894047d88bd6e10cd5f3da5bd4fa4b0ae
Vnuk666 <ya.velesov@gmail.com>
2022-04-13 18:50:16 +0700

    Поздоровались

---
+++ Example005_ConditonIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "люба")
+{
+    Console.WriteLine("Ура это Любимая");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
