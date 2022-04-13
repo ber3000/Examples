f922bf550f289b9338e428170cadc2fcd2ccf302
Vnuk666 <ya.velesov@gmail.com>
2022-04-13 18:31:17 +0700

    добавили Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,5 @@
 ﻿int numberA = new Random().Next(1, 10); //Random- Рандомное число от 1 до 10
-Console,WriteLine(numberA);
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
